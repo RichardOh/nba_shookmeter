e88b14b71c9a293b9383f0c1234dc6b6b2f904b6
Richard Oh <richardoh123@gmail.com>
2017-11-07 23:48:02 -0800

    Extracted free throw rows

--- temp.py
+++ temp.py
@@ -3,7 +3,10 @@
 import pandas as pd
 
 df = pd.read_csv("0021600001.csv")
-
-for str in df['HOMEDESCRIPTION'].iteritems():
-    if str[1].find("Free Throw") != -1:
-        print(str)
+for row in df.itertuples():
+    if not type(row[6]) is float:
+        if row[6].find("Free Throw") != -1:
+            print(row[6])
+    if not type(row[33]) is float:
+        if row[33].find("Free Throw") != -1:
+            print(row[33])
