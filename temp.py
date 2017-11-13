381ac3973f5e79613f315abc22ebf77c90ffe762
Keenan Mach <kmach@ucsd.edu>
2017-11-12 17:53:28 -0800

    retrieve player's shots/fts

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-
+player = 'Kyrie Irving'
 df = pd.read_csv("0021600001.csv")
-df_home = df[df["HOMEDESCRIPTION"].str.contains("Free Throw") == True]["HOMEDESCRIPTION"]
-df_away = df[df["VISITORDESCRIPTION"].str.contains("Free Throw") == True]["VISITORDESCRIPTION"]
+df = df[ df['PLAYER1_NAME'].str.contains(player) == True]
+df = df[ df['EVENTMSGTYPE'].between(1,3) ]
